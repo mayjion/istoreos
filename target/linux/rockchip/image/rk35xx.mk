fd5b1a8226503c6836ee7c7baa704ee6d99a20ef
mayjion <461753544@qq.com>
2025-04-12 12:44:06 +0800

    add ht2

--- target/linux/rockchip/image/rk35xx.mk
+++ target/linux/rockchip/image/rk35xx.mk
@@ -521,3 +521,15 @@ $(call Device/rk3308)
   DEVICE_PACKAGES := kmod-usb-net-rtl8152 ethtool kmod-rkwifi-bcmdhd rkwifi-firmware-ap6256 kmod-sound-soc-rk3308
 endef
 # TARGET_DEVICES += armsom_p2-pro
+
+define Device/hinlink_ht2
+ $(call Device/rk3528)
+   DEVICE_VENDOR := Hinlink
+   DEVICE_MODEL := HT2
+   DEVICE_DTS := rk3528-hinlink-ht2
+   SUPPORTED_DEVICES := hinlink,ht2
+   UBOOT_DEVICE_NAME := easepi-rk3528
+   DEVICE_PACKAGES := kmod-r8168 kmod-thermal kmod-drm kmod-sound-soc-rockchip kmod-brcmfmac kmod-bluetooth kmod-hci-uart
+   KERNEL_CONFIG := config-hinlink_ht2
+ endef
+ TARGET_DEVICES += hinlink_ht2
